74ce19a79e1de879886aca0d4cb2352bae91c5fc
ryuyr <ryu@ryu-desktop.dragon>
2021-05-04 17:22:04 -0700

    added pencil blade

---
+++ src/main/java/com/winterfol/silver/item/PencilBladeItem.java
@@ -0,0 +1,88 @@
+
+package com.winterfol.silver.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.util.ITooltipFlag;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+
+import com.winterfol.silver.procedures.PencilBladeLivingEntityIsHitWithToolProcedure;
+import com.winterfol.silver.SilverModElements;
+
+@SilverModElements.ModElement.Tag
+public class PencilBladeItem extends SilverModElements.ModElement {
+	@ObjectHolder("silver:pencil_blade")
+	public static final Item block = null;
+	public PencilBladeItem(SilverModElements instance) {
+		super(instance, 14);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 512;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 1f;
+			}
+
+			public int getHarvestLevel() {
+				return 3;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(Items.IRON_INGOT, (int) (1)), new ItemStack(Items.DIAMOND, (int) (1)));
+			}
+		}, 3, -2.8f, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+				super.addInformation(itemstack, world, list, flag);
+				list.add(new StringTextComponent("Deals devastating damage when attacking from behind."));
+			}
+
+			@Override
+			public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+				boolean retval = super.hitEntity(itemstack, entity, sourceentity);
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+				World world = entity.world;
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("entity", entity);
+					$_dependencies.put("sourceentity", sourceentity);
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", y);
+					$_dependencies.put("z", z);
+					$_dependencies.put("world", world);
+					PencilBladeLivingEntityIsHitWithToolProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
+		}.setRegistryName("pencil_blade"));
+	}
+}
